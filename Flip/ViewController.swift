be69f75ae866a7efff3319ef8a70d606aa8a2feb
= <seimith@gmail.com>
2017-06-07 11:20:35 -0400

    Reset when app is in background; Add shake

--- Flip/ViewController.swift
+++ Flip/ViewController.swift
@@ -13,9 +13,11 @@ class ViewController: UIViewController {
     var frontCoin: UIImageView!
     var backCoin: UIImageView!
     var scrimView: UIView!
+    var isFlipping: Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.becomeFirstResponder()
         self.view.backgroundColor = UIColor.white
 
         frontCoin = UIImageView(frame: CGRect(x: (self.view.frame.size.width / 2) - 50, y: (self.view.frame.size.height / 2) - 50, width: 100, height: 100));
@@ -48,29 +50,38 @@ class ViewController: UIViewController {
         swipeUp.direction = UISwipeGestureRecognizerDirection.up
         self.view.addGestureRecognizer(swipeUp)
         
-        
+        // Center X
         NSLayoutConstraint(item: frontCoin, attribute: .centerX, relatedBy: .equal, toItem: backCoin, attribute: .centerX, multiplier: 1, constant: 0).isActive = true
-        
+        // Center Y
         NSLayoutConstraint(item: backCoin, attribute: .centerY, relatedBy: .equal, toItem: frontCoin, attribute: .centerY, multiplier: 1, constant: 0).isActive = true
+        
+        // Check if app is becoming INACTIVE
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(appIsInactive), name: NSNotification.Name.UIApplicationWillResignActive, object: nil)
+    }
+    
+    func appIsInactive() {
+        if (self.frontCoin.isHidden) {
+            let transitionOptions: UIViewAnimationOptions = [.transitionFlipFromBottom, .showHideTransitionViews]
+            
+            UIView.transition(with: frontCoin, duration: 0.25, options: transitionOptions, animations: {
+                self.frontCoin.isHidden = !self.frontCoin.isHidden
+            })
+            
+            UIView.transition(with: backCoin, duration: 0.25, options: transitionOptions, animations: {
+                self.backCoin.isHidden = !self.backCoin.isHidden
+            })
+        }
     }
     
     func respondToSwipeGesture(gesture: UIGestureRecognizer) {
         if let swipeGesture = gesture as? UISwipeGestureRecognizer {
             switch swipeGesture.direction {
-            case UISwipeGestureRecognizerDirection.right:
-                print("Swiped right")
-            case UISwipeGestureRecognizerDirection.down:
-                print("Swiped down")
-            case UISwipeGestureRecognizerDirection.left:
-                print("Swiped left")
             case UISwipeGestureRecognizerDirection.up:
-                print("Swiped up")
                 perform(#selector(flip), with: nil, afterDelay: 0);
-                
                 scrimView = UIView(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.height));
                 scrimView.backgroundColor = UIColor.clear
                 view.addSubview(scrimView);
-                
                 let tap = UITapGestureRecognizer(target: self, action: #selector(stop))
                 scrimView.addGestureRecognizer(tap)
             default:
@@ -79,24 +90,37 @@ class ViewController: UIViewController {
         }
     }
     
-    func stop() {
-        print("STOP")
-        self.frontCoin.layer.removeAllAnimations()
-        self.backCoin.layer.removeAllAnimations()
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        if motion == .motionShake {
+            if self.isFlipping == true {
+                perform(#selector(stop), with: nil, afterDelay: 0);
+            } else {
+                perform(#selector(flip), with: nil, afterDelay: 0);
+            }
+        }
     }
 
     func flip() {
-        
-        print("FLIP")
-        let transitionOptions: UIViewAnimationOptions = [.transitionFlipFromBottom, .showHideTransitionViews, .repeat]
-        
-        UIView.transition(with: frontCoin, duration: 0.25, options: transitionOptions, animations: {
-            self.frontCoin.isHidden = !self.frontCoin.isHidden
-        })
-        
-        UIView.transition(with: backCoin, duration: 0.25, options: transitionOptions, animations: {
-            self.backCoin.isHidden = !self.backCoin.isHidden
-        })
+        if self.isFlipping == false {
+            self.isFlipping = true
+            let transitionOptions: UIViewAnimationOptions = [.transitionFlipFromBottom, .showHideTransitionViews, .repeat]
+            UIView.transition(with: frontCoin, duration: 0.25, options: transitionOptions, animations: {
+                self.frontCoin.isHidden = !self.frontCoin.isHidden
+            })
+            UIView.transition(with: backCoin, duration: 0.25, options: transitionOptions, animations: {
+                self.backCoin.isHidden = !self.backCoin.isHidden
+            })
+        }
+    }
+    
+    func stop() {
+        // Settimeout
+        let when = DispatchTime.now() + 0.5
+        DispatchQueue.main.asyncAfter(deadline: when) {
+            self.isFlipping = false
+            self.frontCoin.layer.removeAllAnimations()
+            self.backCoin.layer.removeAllAnimations()
+        }
     }
     
     override func didReceiveMemoryWarning() {
